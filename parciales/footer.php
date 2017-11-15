6d11cfecd79ff9ecc2c72efe86aa72595ab992e9
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2017-11-15 12:37:27 -0600

    estructura php y slider

---
+++ parciales/footer.php
@@ -0,0 +1,24 @@
+
+
+    <footer>
+      <div class="row">
+        <div class="col-sm-12 col-md-6">
+          <h2>mapa</h2>
+        </div>
+        <div class="col-sm-12 col-md-6">
+          <h2>contacto</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-8">
+          <h6>desarrollado por  @jefes.com </h6>
+        </div>
+      </div>
+
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
