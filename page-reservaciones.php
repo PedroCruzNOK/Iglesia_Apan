28505a510fd2f70b033aedcce88a2717eb817cbe
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2017-11-17 15:25:27 -0600

    page reservaciones

---
+++ page-reservaciones.php
@@ -0,0 +1,47 @@
+<?php include("parciales/header.php"); ?>
+<br>
+  <section class="reservaciones">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Fecha</th>
+                <th scope="col">Solicitante</th>
+                <th scope="col">Servicio</th>
+                <th scope="col">Fecha de reserva</th>
+                <th scope="col">correo y telefono</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>27/04/2015</td>
+                <td>Mark</td>
+                <td>Boda</td>
+                <td>01/01/2018</td>
+                <td>7751255294 pedroforo_rey@gmail.com</td>
+              </tr>
+              <tr>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<br>
+<?php include("parciales/footer.php"); ?>
