05da025b0965d586be4dd1ed978368eeaebd8f63
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2017-11-21 14:49:15 -0600

    archivo php para eviar correos electronicos

---
+++ enviar.php
@@ -0,0 +1,10 @@
+<?php
+$destino= "apan@gmail.com";
+$nombre= $_POST["Nombre"];
+$correo= $_POST["Correo"];
+$telefono= $_POST["Telefono"];
+$mensaje= $_POST["Mensaje"];
+$contenido= "Nombre:  " . $nombre . "\nCorreo:  " . $correo . "\nTelefono:  " . $telefono . "\nMensaje:  " . $mensaje;
+mail($destino, "Contacto", $contenido);
+header("Location:http:iglesiaApan.com")
+ ?>
--- parciales/footer.php
+++ parciales/footer.php
@@ -6,25 +6,30 @@
         <h2 class="text-center">Contacto</h2>
         <br>
         <div class="row">
-          <div class="col-sm-12 col-md-6">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5467.419175802243!2d-98.45553716655353!3d19.71303774614428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d034e163ebd619%3A0x60621b64d0e8cb27!2sApan%2C+Estado+de+Hidalgo!5e0!3m2!1ses-419!2smx!4v1510773062194" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-          </div>
+          <div class="col-md-6 text-center">
+            <br>
+            <h4>Av. Domingo Muñoz Chimalpain 92, Tadeo de Niza,
+        43996 Cd Sahagún, Hgo.</h4>
+        <br>
+            <div class="row">
+              <div class="col-md-12">
+
+                <iframe style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4464.870978211661!2d-98.57076241258046!3d19.776641561334856!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTnCsDQ2JzM0LjYiTiA5OMKwMzQnMDMuNSJX!5e0!3m2!1ses!2smx!4v1487613407336" width="450" height="350" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+
+              </div>
+            </div>
+    </div>
           <div class="col-sm-12 col-md-6">
 
-             <form class="formulario-contacto">
-               <div class="form-group">
-                 <label for="email">Email:</label>
-                 <input type="email" class="form-control" id="email" placeholder="Enter email">
-               </div>
-               <div class="form-group">
-                 <label for="pwd">Mensaje:</label>
-                 <input type="email" class="form-control" id="pwd" placeholder="Enter Message">
-               </div>
-               <div class="checkbox">
-                 <label><input type="checkbox"> Recordarme</label>
-               </div>
-               <button type="submit" class="btn btn1 btn-default">Enviar</button>
-             </form>
+            <form class="formulario" action="enviar.php" method="post">
+              <br>
+              <input class="inputFormulario"type="text" name="Nombre" value="" placeholder="Nombre" required="">
+              <input class="inputFormulario"type="text" name="Correo" value="" placeholder="Correo" required="">
+              <input class="inputFormulario"type="text" name="Telefono" value="" placeholder="Telefono" required="">
+              <textarea class="inputFormulario textFormulario"name="Mensaje" rows="8" cols="40" placeholder="Dejenos su mensaje" required=""></textarea>
+              <input class="inputFormulario btn1"type="submit" name="envir" value="Enviar" id="botonn">
+            </form>
+        </div>
           </div>
         </div>
       </div>
